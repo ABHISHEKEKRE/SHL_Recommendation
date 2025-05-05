e7715eb4e4d8bc852857484f6488df9ec3f2a6a1
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-05-05 22:43:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ python -m spacy download en_core_web_sm
 
 # Start the FastAPI app with gunicorn
 gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:$PORT
-=======
+
 gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:$PORT
