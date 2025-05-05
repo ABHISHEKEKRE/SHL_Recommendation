e5da975ebad979cd92b3e933e607c0601f69da8f
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-05-05 22:02:50 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install SpaCy model
+python -m spacy download en_core_web_sm
+
+# Start the FastAPI app with gunicorn
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:$PORT
